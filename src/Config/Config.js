042d7558aa1ed72f0576e0b2970d40a0fce5ac3d
Om Swami <omswami2004@gmail.com>
2024-02-10 11:39:39 +0530

    edited config

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,24 +1,41 @@
-import * as firebase from 'firebase'
 
-import 'firebase/storage';
-import 'firebase/firestore';
-import 'firebase/auth'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import "firebase/compat/database";
+import "firebase/compat/storage";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDGmVBx5LAYJuIq_Ni1nZBenfcVUFX-SyY",
-    authDomain: "ecommerce-with-react-2ac06.firebaseapp.com",
-    databaseURL: "https://ecommerce-with-react-2ac06.firebaseio.com",
-    projectId: "ecommerce-with-react-2ac06",
-    storageBucket: "ecommerce-with-react-2ac06.appspot.com",
-    messagingSenderId: "690603499200",
-    appId: "1:690603499200:web:09860bc318b5b7fd74d725",
-    measurementId: "G-88N35MC51Q"
-};
 
-firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
-const db = firebase.firestore();
-const storage = firebase.storage();
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-export { auth, db, storage }
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyARBVjspDEzSxpJz7WaTU-KZHsD0RugPfE",
+    authDomain: "genbackend.firebaseapp.com",
+    projectId: "genbackend",
+    storageBucket: "genbackend.appspot.com",
+    messagingSenderId: "111630538013",
+    appId: "1:111630538013:web:636c09da99b52d3c71bdf4",
+    measurementId: "G-72GW2PKEVX"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  
+  // Use this to initialize the firebase App
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  
+  // Use these for db & auth
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const storage = firebase.storage();
+  
+  export { auth, db, storage };
